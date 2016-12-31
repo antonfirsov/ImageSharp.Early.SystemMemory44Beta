ad86e1b5876ec4c0bcb57206c54a8d722f11710d
Anton Firszov <anton.firszov@sapagroup.com>
2016-12-31 03:36:31 +0100

    ImageSharp.Early.Unsafe44Beta

---
+++ src/System.Memory/SpanPlz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpanPlz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lol = { 1, 2, 3, 4, 5, 6 };
+            Span<int> span1 = new Span<int>(lol);
+            var span2 = span1.Slice(2, 4);
+
+            for (int i = 0; i < span2.Length; i++)
+            {
+                Console.WriteLine(span2[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
